e2c265b1647f473116cfccee8357808886d84045
Austin Miller <austin.miller1424@gmail.com>
2023-03-30 19:27:37 -0500

    added more books and some colors

--- assets/script.js
+++ assets/script.js
@@ -1,71 +1,148 @@
 const books = [
     {
-        title: "Book 1",
-        description: "This is a description for book 1.",
+        title: "Slay the Narcissist",
+        subtitle: "",
+        description: "Slay narcissists—the ultimate bullies—in negotiation by following this simple formula, developed by a top attorney, which takes you from feeling like a victim to being victorious! Whether your relationship is business or personal, narcissists have a way of making you feel like you are so special. The relationship begins with you believing they are the most charming, charismatic person you’ve ever met, and ends up being one in which hell actually would be a relief. Their skill for reading people is unparalleled. Their knack for honing in on their prey is cunning. The path into hell is camouflaged, artfully concealed under lies and charm, red flags skillfully diverted away, until you finally realize that their tactics have left you feeling utterly drained to your soul. The population of narcissists is becoming an epidemic, and thus, we can no longer continue to ignore it. The problem is that we’ve been applying a blanket approach to negotiation and communication with narcissists and expecting them to work like they do with reasonable people. But narcissists’ brains are not wired the same as reasonable people and therefore they do not think in the same way. That is why a conventional approach to negotiation always fails.But there wasn’t a playbook on HOW to deal with them…until now.Imagine you could communicate with narcissists in a way that leaves you feeling accomplished instead of fearful of what they are going to do, hide, or lie about next. You could transform your relationship with conflict and live life the way you want to. Rebecca Zung’s revolutionary SLAY Methodology will show you how to handle with narcissists once and for all, and once you conquer them anything is possible. This about you feeling valued, honored, and respected and setting yourself up so nothing and no one can ever infringe on that. After twenty years of dealing with narcissists as an attorney (including as clients and opposing clients) and unfortunately, dealing with them in her personal life, Rebecca has a deep understanding of a narcissist’s psyche, which she’s now turned into a simple step by step process to get what you want. In this book, Zung shares her framework to SLAY your negotiation with the narcissist. By the time you’re finished reading, you will know how to shift the narrative, the conversation, and the dynamic of power and be more confident and empowered in every aspect of your life!",
         isbn: "1234567890",
-        author: "Author 1",
-        genre: "Genre 1",
-        coverUrl: "images/rZung.webp"
+        author: "Rebecca Zung",
+        genre: "Business Negotiation",
+        coverUrl: "images/rZung.webp",
+        releaseDate:"October 3, 2023"
     },
     {
-        title: "Book 2",
+        title: "Daily Alignment",
+        subtitle: "",
         description: "This is a description for book 2.",
         isbn: "1234567899",
-        author: "Author 2",
+        author: "Lucy Byrd Hope",
         genre: "Genre 2",
-        coverUrl: "images/rZung.webp"
+        coverUrl: "images/rZung.webp",
+        releaseDate:"March 28, 2023"
     },
     {
-        title: "Book 3",
+        title: "Grace Under Pressure",
+        subtitle: "",
         description: "This is a description for book 3.",
         isbn: "1234567890",
-        author: "Author 3",
+        author: "John Baldoni",
         genre: "Genre 3",
-        coverUrl: "images/rZung.webp"
+        coverUrl: "images/rZung.webp",
+        releaseDate:"April 4, 2023"
     },
     {
-        title: "Book 4",
+        title: "Mind Golf",
+        subtitle: "",
         description: "This is a description for book 4.",
         isbn: "1234567899",
-        author: "Author 4",
+        author: "Robert Ragland Young",
         genre: "Genre 4",
-        coverUrl: "images/rZung.webp"
-    }
+        coverUrl: "images/rZung.webp",
+        releaseDate:"May 2, 2023"
+    },
+    {
+        title: "The Moment That Will Define Your Life",
+        subtitle: "",
+        description: "This is a description for book 4.",
+        isbn: "1234567899",
+        author: "Chuck Garcia",
+        genre: "Genre 4",
+        coverUrl: "images/rZung.webp",
+        releaseDate:"February 6, 2024"
+    },
+    {
+        title: "Education is Freedom",
+        subtitle: "",
+        description: "This is a description for book 4.",
+        isbn: "1234567899",
+        author: "Jim Keyes",
+        genre: "Genre 4",
+        coverUrl: "images/rZung.webp",
+        releaseDate:"February 27, 2024"
+    },
+    {
+        title: "The Franchise Accelerato",
+        subtitle: "",
+        description: "This is a description for book 4.",
+        isbn: "1234567899",
+        author: "Anne Gannon",
+        genre: "Genre 4",
+        coverUrl: "images/rZung.webp",
+        releaseDate:"March 19, 2024"
+    },
+
 ];
 
 const backListBooks = [
     {
-        title: "Book 1",
-        description: "This is a description for book 1.",
-        isbn: "1234567890",
-        author: "Author 1",
-        genre: "Genre 1",
-        coverUrl: "images/rZung.webp"
+        title: "When A.I Rules the World",
+        subtitle: "",
+        description: "As China rises to true superpower status, the United States must take immediate action to catch up in AI capabilities—or facefall disastrously behind technologically, economically, and militarily. Over the past decade, China has quietly and methodically moved into a near-leadership position in artificial intelligence technologies on a global scale. Meanwhile, the United States has responded ineffectively, weighed down by politics, bureaucracy, and an absence of clear strategy. In the near future, wars will be fought not over land, but over data. Machines will quickly discover individualized treatments for diseases, and with the help of virtual reality, AI will inspect buildings that have not yet been built. With the rising interest in these technologies by both China and the U.S., who will emerge as the victor of this technological race? When AI Rules the World is an investigation and call to action into AI technologies for a nation that does not yet comprehend the full gravity of the AI revolution. The United States is losing the race for AI dominance, and the stakes couldn’t be higher.transformational activities, and a clear-cut formula",
+        isbn: "1642938122",
+        author: "Handel Jones",
+        genre: "Technology",
+        coverUrl: "images/rZung.webp",
+        releaseDate:"July 29, 2022"
     },
     {
-        title: "Book 2",
-        description: "This is a description for book 2.",
-        isbn: "1234567899",
-        author: "Author 2",
-        genre: "Genre 2",
-        coverUrl: "images/rZung.webp"
+        title: "The Five Elements of Relationships",
+        subtitle: "",
+        description: "Do you ever wonder why some people just rub you the wrong way? Or why you automatically click with others? Or maybe you even ask yourself, “Who am I, really?”Wouldn’t it be wonderful if you could understand why people are the way they are—and even better understand yourself? Now you can! And it’s simple and fun! Based on the ancient Five Elements model from Chinese medicine, Dr. Vicki Matthews has developed a simple way to describe our five basic personality types and the predictable ways in which they interact. Take your first step toward happier and more harmonious relationships in every area of your life—including a better relationship with yourself! The result of years of personal and professional experience, this invaluable book can improve every relationship you have and make your life better!",
+        isbn: "1637584512",
+        author: "Dr. Viicki Matthews",
+        genre: " Relationships",
+        coverUrl: "images/rZung.webp",
+        releaseDate:"July 5th, 2022"
     },
     {
-        title: "Book 3",
-        description: "This is a description for book 3.",
-        isbn: "1234567890",
-        author: "Author 3",
-        genre: "Genre 3",
-        coverUrl: "images/rZung.webp"
+        title: "Don't Get Mad at Penguins",
+        subtitle: "",
+        description: "An indispensable tool kit for engaging and harnessing healthy conflict to drive success and happiness, and eliminate the pain and stress of toxic conflict. While most people view conflict as a bad thing that should be avoided at all costs, the reality is that embracing conflict in a healthy way is the single greatest factor in driving success and avoiding painful failure. Don’t Get Mad at Penguins breaks down the factors that escalate conflict to dangerous heights and shows us how to defuse conflict and make it work for us rather than against us. Just as toxins in our bodies make us unhealthy, slow us down, and cause us pain, the same holds true for toxins in our organizations and minds. They cause healthy conflict to become destructive, which then consumes an organization’s energy and taxes its ability to compete, grow, and prosper. Team engagements become pained, client relationships are strained, and individual careers suffer. The same unhealthy, painful dynamics play outin our personal lives as well. The key to making conflict work for our benefit lies in removing toxins from our interactions, and Don’t Get Mad at Penguins offers simple yet powerfully effective tools to do just that.",
+        isbn: "1637581637",
+        author: "Gabe Karp",
+        genre: "Buisness Leadership",
+        coverUrl: "images/rZung.webp",
+        releaseDate:""
     },
     {
-        title: "Book 4",
-        description: "This is a description for book 4.",
-        isbn: "1234567899",
-        author: "Author 4",
-        genre: "Genre 4",
-        coverUrl: "images/rZung.webp"
-    }
+        title: "The Life Ledger",
+        subtitle: "",
+        description: "The Life Ledger is the applicable formula you need to achieve your personal, professional, and financial goals to guarantee your success. You’ve heard it all. Put positive thoughts into the world and success will come to you. Take risks or you’ll never progress. Focus on goals, not dreams. Visualize your success and it will happen. There are thousands of books, mentors, seminars, life coaches, and online courses out there with a thousand assortments of different platitudes. These resources talk about motivation, inspiration, mindset, attitude, and setting lofty goals. Sure, it will give you the warm and fuzzies—but deep down, you know it’s all self-help-bullshit. They don’t give you the steps or the tools to implement what they are trying to convey. Instead of listening to someone who tells you what they think you need to hear, listen to someone who has real experience with changing their life through accountability. Listen to someone who has seen the inner workings of companies, who knows what makes them tick and the reasons why they stagnate, grow, and thrive. It’s time to ditch the self-help books and give your attention, time, and resources to an expert. Me. Let’s develop the ledger of your life. ",
+        isbn: "1637583893",
+        author: "J.D Frost",
+        genre: "Sucess Self-Help",
+        coverUrl: "images/rZung.webp",
+        releaseDate:""
+    },
+    {
+        title: "STAND ALONE: How to be and Extraordinary Leader",
+        subtitle: "",
+        description: "So many people are telling you what you want to hear. Finally someone is saying what we all need to hear. The truth about success is breathtaking and needed now more than ever. This book demonstrates what it really takes to succeed. Learn how to overcome obstacles and create the life you want by becoming the leader you were meant to be. Stand Alone will help you see how you can go from living an ordinary life—where life just happens to you—to living a life of significance and influence.",
+        isbn: "1637582951",
+        author: "Sabrina Lloyd",
+        genre: "Buisness Leadership",
+        coverUrl: "images/rZung.webp",
+        releaseDate:""
+    },
+    {
+        title: "Empower Your Investing: Adopting Best Practices From John Templeton, Peter Lynch, and Warren Buffett",
+        subtitle: "",
+        description: "An indispensable tool kit for engaging and harnessing healthy conflict to drive success and happiness, and eliminate the pain and stress of toxic conflict. While most people view conflict as a bad thing that should be avoided at all costs, the reality is that embracing conflict in a healthy way is the single greatest factor in driving success and avoiding painful failure. Don’t Get Mad at Penguins breaks down the factors that escalate conflict to dangerous heights and shows us how to defuse conflict and make it work for us rather than against us. Just as toxins in our bodies make us unhealthy, slow us down, and cause us pain, the same holds true for toxins in our organizations and minds. They cause healthy conflict to become destructive, which then consumes an organization’s energy and taxes its ability to compete, grow, and prosper. Team engagements become pained, client relationships are strained, and individual careers suffer. The same unhealthy, painful dynamics play outin our personal lives as well. The key to making conflict work for our benefit lies in removing toxins from our interactions, and Don’t Get Mad at Penguins offers simple yet powerfully effective tools to do just that.",
+        isbn: "1642932388",
+        author: "Gabe Scott a Chapman",
+        genre: "International Investing",
+        coverUrl: "images/rZung.webp",
+        releaseDate:""
+    },
+    {
+        title: "THERE'S NO FREE LUNCH: 250 Economic Truths",
+        subtitle: "",
+        description: "The best way to defend the cause of human flourishing against this current onslaught of dangerous economic thinking is to relearn time-tested economic truths. The verdict is in: Free enterprise has lifted billions of people out of abject poverty all over the world and provided a higher quality of life than has ever been thought possible. But a growing case is forming in public opinion against free markets, and for a significantly larger command & control management of the economy. Whether you call it socialism or progressive leftism, more and more people are turning away from the forces of freedom and social cooperation that made the last two hundred years of prosperity possible, and embracing a system that deprives human beings of their dignity, impoverishing whole societies both financially and spiritually. What David Bahnsen does here is pull from the masters—the great economic voices of the past and the present—to remind readers of the basic economic truths that must serve as our foundation in understanding the challenges of today. In 250 vital points, he combines pearls of wisdom from economic legends with his own careful commentary to provide readers the perspective, information, and reaffirmation they need in order to see economics for what it is. It will empower you and equip you with the truth—250 truths—that are crucially needed to keep the lights on in civilization and advance the cause of human flourishing.",
+        isbn: "1637580142",
+        author: "David Bahnsen",
+        genre: "Business Economics",
+        coverUrl: "images/rZung.webp",
+        releaseDate:""
+    },
 ];
 
 const bookContainer = document.getElementById("book-container");
@@ -84,6 +161,11 @@ books.forEach(book => {
     bookTitle.className = "book-title";
     bookTitle.textContent = book.title;
     bookDiv.appendChild(bookTitle);
+
+    const bookSubtitle = document.createElement("h2");
+    bookSubtitle.className = "book-Subtitle";
+    bookSubtitle.textContent = book.subtitle;
+    bookDiv.appendChild(bookSubtitle);
 
     const bookDescription = document.createElement("p");
     bookDescription.className = "book-details";
