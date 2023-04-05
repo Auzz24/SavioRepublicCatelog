c051196921d1172ef913f0700740d4d9fb2a65e5
Austin Miller <austin.miller1424@gmail.com>
2023-04-04 21:03:15 -0500

    formmating and added new books

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,26 @@ const books = [
     {
         title: "SLAY the Bully",
         subtitle: "How to Negotiate with a Narcissist and Win",
-        description: "Slay narcissists—the ultimate bullies—in negotiation by following this simple formula, developed by a top attorney, which takes you from feeling like a victim to being victorious! Whether your relationship is business or personal, narcissists have a way of making you feel like you are so special. The relationship begins with you believing they are the most charming, charismatic person you’ve ever met, and ends up being one in which hell actually would be a relief. Their skill for reading people is unparalleled. Their knack for honing in on their prey is cunning. The path into hell is camouflaged, artfully concealed under lies and charm, red flags skillfully diverted away, until you finally realize that their tactics have left you feeling utterly drained to your soul. The population of narcissists is becoming an epidemic, and thus, we can no longer continue to ignore it. The problem is that we’ve been applying a blanket approach to negotiation and communication with narcissists and expecting them to work like they do with reasonable people. But narcissists’ brains are not wired the same as reasonable people and therefore they do not think in the same way. That is why a conventional approach to negotiation always fails.",
+        description: "Slay narcissists—the ultimate bullies—in negotiation by following this simple formula, developed by a top attorney, which takes you from feeling like a victim to being victorious! Whether your relationship is business or personal, narcissists have a way of making you feel like you are so special. The relationship begins with you believing they are the most charming, charismatic person you’ve ever met, and ends up being one in which hell actually would be a relief. Their skill for reading people is unparalleled. Their knack for honing in on their prey is cunning. The population of narcissists is becoming an epidemic, and thus, we can no longer continue to ignore it. The problem is that we’ve been applying a blanket approach to negotiation and communication with narcissists and expecting them to work like they do with reasonable people. But narcissists’ brains are not wired the same as reasonable people and therefore they do not think in the same way. That is why a conventional approach to negotiation always fails.",
         descriptionTwo: "But there wasn’t a playbook on HOW to deal with them…until now.",
-        descriptionThree: " Imagine you could communicate with narcissists in a way that leaves you feeling accomplished instead of fearful of what they are going to do, hide, or lie about next. You could transform your relationship with conflict and live life the way you want to. Rebecca Zung’s revolutionary SLAY Methodology will show you how to handle with narcissists once and for all, and once you conquer them anything is possible. This about you feeling valued, honored, and respected and setting yourself up so nothing and no one can ever infringe on that. After twenty years of dealing with narcissists as an attorney (including as clients and opposing clients) and unfortunately, dealing with them in her personal life, Rebecca has a deep understanding of a narcissist’s psyche, which she’s now turned into a simple step by step process to get what you want. In this book, Zung shares her framework to SLAY your negotiation with the narcissist. By the time you’re finished reading, you will know how to shift the narrative, the conversation, and the dynamic of power and be more confident and empowered in every aspect of your life!",
+        descriptionThree: " Imagine you could communicate with narcissists in a way that leaves you feeling accomplished instead of fearful of what they are going to do next. You could transform your relationship with conflict and live life the way you want to. Rebecca Zung’s revolutionary SLAY Methodology will show you how to handle with narcissists once and for all, and once you conquer them anything is possible. This about you feeling valued, honored, and respected and setting yourself up so nothing and no one can ever infringe on that. After twenty years of dealing with narcissists as an attorney and unfortunately, dealing with them in her personal life, Rebecca has a deep understanding of a narcissist’s psyche. She has that understanding turned into a simple step by step process to get what you want.",
         isbn: "1234567890",
         author: "Rebecca Zung",
         genre: "Business Negotiation",
         coverUrl: "images/rZung.webp",
         releaseDate:"October 3, 2023"
+    },
+    {
+        title: "Stand Alone",
+        subtitle: "How to be and Extraordinary Leader",
+        description: "NEW YORK TIMES BESTSELLER IN BUSINESS",
+        descriptionTwo:"So many people are telling you what you want to hear....Finally someone is saying what we all need to hear. The truth about success is breathtaking and needed now more than ever. This book demonstrates what it really takes to succeed. Learn how to overcome obstacles and create the life you want by becoming the leader you were meant to be. Stand Alone will help you see how you can go from living an ordinary life—where life just happens to you—to living a life of significance and influence.",
+        descriptionThree:"You will The importance of acceptance and transformation in leadership, and how to develop these qualities in yourself. You wil learn strategies for building a strong and diverse team that can weather any challenge. You will also learn Practical tips for setting goals, managing your time, and staying focused on what truly matters. Sabrina Lloyd is an entrepreneur, executive coach, and multi-millionaire living in Chicago. She leads with courage and strength and works to equip people with what they really need to succeed. Using the right tools and knowledge, she empowers people to become the courageous and strong leaders they were meant to be.",
+        isbn: "1637582951",
+        author: "Sabrina Lloyd",
+        genre: "Buisness Leadership",
+        coverUrl: "images/standAlone.jpg",
+        releaseDate:"October 25, 2022"
     },
     {
         title: "Daily Alignment",
@@ -37,45 +49,28 @@ const books = [
     },
     {
         title: "Mind Golf",
-        subtitle: "",
-        description: "This is a description for book 4.",
+        subtitle: "The Troubled Genius of Moe Norman",
+        description: "This may be the most unusual golf book ever written.",
+        descriptionTwo: "At one of a series of clinics that former USPGA teacher of the year Craig Shankland staged, he asked Moe Norman in front of about 300 people, “What’s it like to hit perfect shots, Moe?” Moe paused and looked at the audience. Then, in jest, said, “You will never know.”",
+        descriptionThree:"Mind Golf assembles and ignites the mental energy of the image of the shot-to-be. Moe “Pipeline Moe” Norman was a Canadian professional golfer and the best ball-striker the world has ever known. Author Bob Young met the eccentric golfer and traveled between Canada and Florida from the early 1960s to 2004, where he was able to observe Pipeline Moe for decades. Mind Golf is about life and golf and the metaphysical connection between Norman’s mechanics and Young’s perception of the mental construct of ball striking. The core of Mind Golf recounts the trail to Young’s discovery, which will help you discover it too.",
         isbn: "1234567899",
         author: "Robert Ragland Young",
-        genre: "Genre 4",
-        coverUrl: "images/rZung.webp",
+        genre: "Golf",
+        coverUrl: "images/mindGolf.webp",
         releaseDate:"May 2, 2023"
     },
     {
-        title: "The Moment That Will Define Your Life",
-        subtitle: "",
-        description: "This is a description for book 4.",
-        isbn: "1234567899",
-        author: "Chuck Garcia",
-        genre: "Genre 4",
-        coverUrl: "images/rZung.webp",
-        releaseDate:"February 6, 2024"
+        title: "The Life Ledger",
+        subtitle: "How to Build a System to Reach Your Goals",
+        description: "The Life Ledger is the applicable formula you need to achieve your personal, professional, and financial goals to guarantee your success.",
+        descriptionTwo: "You’ve heard it all. Put positive thoughts into the world and success will come to you. Take risks or you’ll never progress. Focus on goals, not dreams. Visualize your success and it will happen. There are thousands of books, mentors, seminars, life coaches, and online courses out there with a thousand assortments of different platitudes. These resources talk about motivation, inspiration, mindset, attitude, and setting lofty goals. Sure, it will give you the warm and fuzzies—but deep down, you know it’s all self-help-bullshit. They don’t give you the steps or the tools to implement what they are trying to convey. Instead of listening to someone who tells you what they think you need to hear, listen to someone who has real experience with changing their life through accountability. Listen to someone who has seen the inner workings of companies, who knows what makes them tick and the reasons why they stagnate, grow, and thrive. It’s time to ditch the self-help books and give your attention, time, and resources to an expert. Me. Let’s develop the ledger of your life.",
+        descriptionThree:"",
+        isbn: "1637583893",
+        author: "J.D Frost",
+        genre: "Sucess Self-Help",
+        coverUrl: "images/lifeLedger.webp",
+        releaseDate:"ovember 29, 2022"
     },
-    {
-        title: "Education is Freedom",
-        subtitle: "",
-        description: "This is a description for book 4.",
-        isbn: "1234567899",
-        author: "Jim Keyes",
-        genre: "Genre 4",
-        coverUrl: "images/rZung.webp",
-        releaseDate:"February 27, 2024"
-    },
-    {
-        title: "The Franchise Accelerato",
-        subtitle: "",
-        description: "This is a description for book 4.",
-        isbn: "1234567899",
-        author: "Anne Gannon",
-        genre: "Genre 4",
-        coverUrl: "images/rZung.webp",
-        releaseDate:"March 19, 2024"
-    },
-
 ];
 
 const backListBooks = [
@@ -105,26 +100,6 @@ const backListBooks = [
         description: "An indispensable tool kit for engaging and harnessing healthy conflict to drive success and happiness, and eliminate the pain and stress of toxic conflict. While most people view conflict as a bad thing that should be avoided at all costs, the reality is that embracing conflict in a healthy way is the single greatest factor in driving success and avoiding painful failure. Don’t Get Mad at Penguins breaks down the factors that escalate conflict to dangerous heights and shows us how to defuse conflict and make it work for us rather than against us. Just as toxins in our bodies make us unhealthy, slow us down, and cause us pain, the same holds true for toxins in our organizations and minds. They cause healthy conflict to become destructive, which then consumes an organization’s energy and taxes its ability to compete, grow, and prosper. Team engagements become pained, client relationships are strained, and individual careers suffer. The same unhealthy, painful dynamics play outin our personal lives as well. The key to making conflict work for our benefit lies in removing toxins from our interactions, and Don’t Get Mad at Penguins offers simple yet powerfully effective tools to do just that.",
         isbn: "1637581637",
         author: "Gabe Karp",
-        genre: "Buisness Leadership",
-        coverUrl: "images/rZung.webp",
-        releaseDate:""
-    },
-    {
-        title: "The Life Ledger",
-        subtitle: "",
-        description: "The Life Ledger is the applicable formula you need to achieve your personal, professional, and financial goals to guarantee your success. You’ve heard it all. Put positive thoughts into the world and success will come to you. Take risks or you’ll never progress. Focus on goals, not dreams. Visualize your success and it will happen. There are thousands of books, mentors, seminars, life coaches, and online courses out there with a thousand assortments of different platitudes. These resources talk about motivation, inspiration, mindset, attitude, and setting lofty goals. Sure, it will give you the warm and fuzzies—but deep down, you know it’s all self-help-bullshit. They don’t give you the steps or the tools to implement what they are trying to convey. Instead of listening to someone who tells you what they think you need to hear, listen to someone who has real experience with changing their life through accountability. Listen to someone who has seen the inner workings of companies, who knows what makes them tick and the reasons why they stagnate, grow, and thrive. It’s time to ditch the self-help books and give your attention, time, and resources to an expert. Me. Let’s develop the ledger of your life. ",
-        isbn: "1637583893",
-        author: "J.D Frost",
-        genre: "Sucess Self-Help",
-        coverUrl: "images/rZung.webp",
-        releaseDate:""
-    },
-    {
-        title: "STAND ALONE: How to be and Extraordinary Leader",
-        subtitle: "",
-        description: "So many people are telling you what you want to hear. Finally someone is saying what we all need to hear. The truth about success is breathtaking and needed now more than ever. This book demonstrates what it really takes to succeed. Learn how to overcome obstacles and create the life you want by becoming the leader you were meant to be. Stand Alone will help you see how you can go from living an ordinary life—where life just happens to you—to living a life of significance and influence.",
-        isbn: "1637582951",
-        author: "Sabrina Lloyd",
         genre: "Buisness Leadership",
         coverUrl: "images/rZung.webp",
         releaseDate:""
